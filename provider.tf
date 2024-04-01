57526cb1feec3c1769928090e39c4a096db4425c
Nixon <nixonlauture@yahoo.com>
2024-04-01 10:05:54 -0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,22 +5,7 @@
  Proprietary and confidential
  */
 
- 
-provider "aws" {
-  region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_access_key
-}
 
-variable "aws_secret_access_key" {
-  description = "The AWS secret key"
-  type        = string
-}
-
-variable "aws_access_key" {
-  description = "The AWS access key"
-  type        = string
-}
 
 terraform {
   required_version = ">= 0.14"
