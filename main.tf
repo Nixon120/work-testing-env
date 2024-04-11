a9227e2e19d9e2e57ddb477534dd1607f31a334f
Nixon <nixonlauture@yahoo.com>
2024-04-11 14:48:04 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*
+
 # Copyright © 2024 Radiology Partners, Inc. - All Rights Reserved
 # Unauthorized copying of this file, via any medium is strictly prohibited
 # Proprietary and confidential
@@ -148,4 +148,4 @@ resource "aws_eip_association" "mirth_eip_assoc" {
   allocation_id = aws_eip.mirth_eip.id
 }
 
-*/
+
